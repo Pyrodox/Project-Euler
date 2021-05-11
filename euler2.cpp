f9f0b74d746b7165d30577e01fc6380240676afd
CPY <SirWilliamFarquadTheFourth@betterthanyou.com>
2021-05-11 15:36:44 -0700

    ex 1 and 2

---
+++ euler2.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <vector>
+using std::cout; using std::endl; using std::vector;
+
+int main()
+{
+    int total = 2, first = 1, second = 2;
+    vector<int> fib_seq;
+    fib_seq.push_back(first);
+    fib_seq.push_back(second);
+    for (int i = 3; i <= 4000000; i = fib_seq[fib_seq.size() - 1] + fib_seq[fib_seq.size() - 2]) {
+        if (i % 2 == 0) {
+            total += i;
+        }
+        fib_seq.push_back(i);
+    }
+    cout << total << endl;
+    return 0;
+}
