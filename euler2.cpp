e80b27e907400020819e818f35bf7512c3ad230d
CPY <SirWilliamFarquadTheFourth@betterthanyou.com>
2021-05-11 17:30:59 -0700

    optimize q2

--- euler2.cpp
+++ euler2.cpp
@@ -1,18 +1,15 @@
 #include <iostream>
-#include <vector>
-using std::cout; using std::endl; using std::vector;
+using std::cout; using std::endl;
 // answer is 4613732
 int main()
 {
     int total = 2, first = 1, second = 2;
-    vector<int> fib_seq;
-    fib_seq.push_back(first);
-    fib_seq.push_back(second);
-    for (int i = 3; i <= 4000000; i = fib_seq[fib_seq.size() - 1] + fib_seq[fib_seq.size() - 2]) {
+    for (int i = 3; i <= 4000000; i = first + second) {
         if (i % 2 == 0) {
             total += i;
         }
-        fib_seq.push_back(i);
+        first = second;
+        second = i;
     }
     cout << total << endl;
     return 0;
