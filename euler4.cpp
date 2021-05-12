b1f65fac69868d0132d251e0ce46d4b8e9480ce4
CPY <SirWilliamFarquadTheFourth@betterthanyou.com>
2021-05-12 16:17:14 -0700

    q5

--- euler4.cpp
+++ euler4.cpp
@@ -9,12 +9,8 @@ int main()
         for (int t = 900; t < 1000; ++t) {
             if(to_string(i * t).size() % 2 == 0) {
                 auto possible = to_string(i * t);
-                if(possible[0] == possible.back()) {
-                    if (possible[1] == possible[possible.length() - 2]) {
-                        if (possible[2] == possible[possible.length() - 3]) {
-                            palindrome = possible;
-                        }
-                    }
+                if(possible[0] == possible.back() && possible[1] == possible[possible.length() - 2] && possible[2] == possible[possible.length() - 3]) {
+                   palindrome = possible;
                 }
             }
         }
---
+++ euler5.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using std::cout;
+
+int main()
+{
+    int count = 0;
+    int a = 0;
+    while (count != 20) {
+        for (int t = 70; t < 999999999; ++t) {
+            if (count == 20) {
+                break;
+            }
+            a = t;
+            for (int i = 1; i <= 20; ++i) {
+                if (t % i == 0) {
+                    ++count;
+                    if (count == 20) {
+                        break;
+                    }
+                    // cout << count;
+                }
+                else {
+                    count = 0;
+                    break;
+                }
+                if (count == 20) {
+                    break;
+                }
+            }
+        }
+    }
+    cout << a;
+
+    return 0;
+}
